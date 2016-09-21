7c9a1b0bbaca76a104ae53be94a85df3d76baabf
Ian <shiny@what.local>
2016-09-20 21:22:39 -0500

    something I guess

--- scripts/main.js
+++ scripts/main.js
@@ -5,38 +5,40 @@ var students = [];
 // var numStudents;
 // var currentStudent = 0;
 
-$(document).ready(function(){
-$('body').on('click', '#startButton', function(){
+$(document).ready(function() {
+  $('body').on('click', '#startButton', function() {
     console.log("start button clicked");
 
-     $.ajax({
-       url: "http://devjana.net/pi/pi_students.json",
-       datatype: "JSON",
-       success: function(data){
-          console.log("in ajax success");
-          console.log(data);
-          students = data.students;
-          console.log(students);
-          var showStudents = function() {
-            console.log("in showStudents");
-            for (var i = 0; i < students.length; i++) {
-              $('#studentName').html('<p><b>' + students[i].first_name + ' ' + students[i].last_name + '</b></p>');
-              $('#studentDesc').html('<p>' + students[i].info + '</p>');
-              showStudents();
-            }
-          };
-         }, // end success
+    $.ajax({
+      url: "http://devjana.net/pi/pi_students.json",
+      datatype: "JSON",
 
-       statusCode: {
-          404: function(){
-             alert("error connecting to server. perhaps try again?");
-          } // end 404
-         } // end statusCode
+      success: function(data) {
+        console.log("in ajax success");
+        console.log(data);
+        students = data.students;
+        console.log(students);
 
+        var i = 0;
+        setInterval(function() {
+            var currentStudent = students[i++];
+            $('#studentName').html('<span><p><b>' + students[i].first_name + ' ' + students[i].last_name + '</b></p></span>');
+            $('#studentDesc').html('<span><p>' + students[i].info + '</p></span>');
+            if(i >= students.length) currentStudent = 0;
+        }, 100);
 
-      }); // end ajax object
-    });
-  }); // end click start button
+              }, // end ajax success
+
+                statusCode: {
+                    404: function() {
+                            alert("error connecting to server. perhaps try again?");
+                        } // end 404
+                } // end statusCode
+
+            }); // end ajax object
+    }); // end click start button
+}); //end document ready
+
 
 // write prev student/button function: currentStudent - 1
 // var prevStudent = function() {
